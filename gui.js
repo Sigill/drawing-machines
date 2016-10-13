de27e903886becce06c3e9b68b63fb702b9889b7
Cyrille Faucheux <cyrille.faucheux@gmail.com>
2016-10-13 22:29:36 +0200

    Remove lower bound on amplitude parameters.

--- gui.js
+++ gui.js
@@ -284,11 +284,11 @@ function SwingingBoardGui(guiFolder, parameters) {
         [ 'period_x', 'phase_x', 'amplitude_x', 'period_y', 'phase_y', 'amplitude_y' ],
         [ guiFolder.add(parameters, 'period_x').min(1).step(1).name("Period X"),
           guiFolder.add(parameters, 'phase_x').min(0).max(2 * Math.PI).step(0.1).name("Phase X"),
-          guiFolder.add(parameters, 'amplitude_x').min(0).name("Amplitude X"),
+          guiFolder.add(parameters, 'amplitude_x').name("Amplitude X"),
 
           guiFolder.add(parameters, 'period_y').min(1).step(1).name("Period Y"),
           guiFolder.add(parameters, 'phase_y').min(0).max(2 * Math.PI).step(0.1).name("Phase Y"),
-          guiFolder.add(parameters, 'amplitude_y').min(0).name("Amplitude Y") ]);
+          guiFolder.add(parameters, 'amplitude_y').name("Amplitude Y") ]);
 }
 
 Extend(SwingingBoardGui, GuiObject);
@@ -299,7 +299,7 @@ function PendulumGui(guiFolder, parameters) {
         [ 'period', 'phase', 'amplitude' ],
         [ guiFolder.add(parameters, 'period').min(1).step(1).name("Period"),
           guiFolder.add(parameters, 'phase').min(0).max(2 * Math.PI).step(0.1).name("Phase"),
-          guiFolder.add(parameters, 'amplitude').min(0).name("Amplitude") ]);
+          guiFolder.add(parameters, 'amplitude').name("Amplitude") ]);
 }
 
 Extend(PendulumGui, GuiObject);
